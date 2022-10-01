5f68b4ef65f23ecd0a6ff412a0a6f07e3b2491e2
jiaotache <45108836+jiaotache@users.noreply.github.com>
2022-10-01 10:20:19 +0900

    changed from S&P 500 to TOPIX

--- trend.py
+++ trend.py
@@ -1,67 +1,58 @@
-# https://www.analyticsvidhya.com/blog/2021/06/download-financial-dataset-using-yahoo-finance-in-python-a-complete-guide/?
-
-import pandas as pd
+#ライブラリのインポート
+from pandas_datareader.stooq import StooqDailyReader
 import datetime as dt
-import yfinance as yf
-from yahoofinancials import YahooFinancials
 import bottleneck as bn
-import matplotlib.pyplot as plt
 import japanize_matplotlib
+import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import streamlit as st
 
+# ma は moving averageの略。
 
-# ma は moving average。
+#株価取得範囲を設定
+start = dt.datetime(int(dt.datetime.now().strftime('%Y')) - 4, 1, 1)
+end = dt.datetime.now()
+#銘柄コードを入力
+#日経平均株価とTopix
+stock = ['^NKX','^DJI','^TPX']
 
-def moving_average(issue_name):
-    dt_today = dt.datetime.now().strftime('%Y-%m-%d')
-    df = yf.download(issue_name,
-                     start='2018-01-01',
-                     end=dt_today,
-                     progress=False,
-                     )
-    df[issue_name + '_3days_ma'] = bn.move_mean(df['Close'], window=3)
-    df = df.rename(columns={'Close': issue_name + '_close'})
-    return df.loc[:, [issue_name + '_close', issue_name + '_3days_ma']]
+#株価取得
+df = StooqDailyReader(stock, start=start, end=end)
+df_stock = df.read()['Close']
 
+df_stock = df_stock.fillna(method='ffill')
+for column in df_stock:
+  df_stock[df_stock[column].name + '_3days_ma'] = bn.move_mean(df_stock[column], window=3)
 
-ma_df = pd.merge(moving_average('^DJI'), moving_average(
-    '^N225'), on='Date', how='outer').sort_values('Date')
-ma_df = pd.merge(ma_df, moving_average('^GSPC'), on='Date',
-                 how='outer').sort_values('Date')
+# print(df_stock.head(15)) 
+# print(df_stock.tail(15))
 
-ma_df_filled = ma_df.fillna(method='ffill')
-
-# https://www.yutaka-note.com/entry/pandas_plot
-# https://www.yutaka-note.com/entry/matplotlib_time_axis#%E6%9C%88%E5%8D%98%E4%BD%8D%E3%81%AE%E7%9B%AE%E7%9B%9B%E3%82%8A
+#matplotlibで株価をグラフ化
 fig = plt.figure()
 
 ax1 = fig.add_subplot()
 ax2 = ax1.twinx()
-ax1.plot(ma_df_filled.index,
-         ma_df_filled['^DJI_3days_ma'], color="orange", label='NYダウ')
-ax1.plot(ma_df_filled.index,
-         ma_df_filled['^N225_3days_ma'], color="blue", label='日経225')
-ax2.plot(ma_df_filled.index,
-         ma_df_filled['^GSPC_3days_ma'], color="green", label='S&P500')
+ax1.plot(df_stock.index,
+         df_stock['^DJI_3days_ma'], color="orange", label='NYダウ(^DJI)')
+ax1.plot(df_stock.index,
+         df_stock['^NKX_3days_ma'], color="blue", label='日経225(^NKX)')
+ax2.plot(df_stock.index,
+         df_stock['^TPX_3days_ma'], color="green", label='TOPIX(^TPX)')
 locator = mdates.MonthLocator(bymonthday=15, interval=2)
 ax1.xaxis.set_major_locator(locator)
 ax1.xaxis.set_tick_params(rotation=45)
 
 ax1.set_ylabel("NYダウ/日経225")  # y1軸ラベル
-ax2.set_ylabel("S&P500")  # y2軸ラベル
+ax2.set_ylabel("TOPIX")  # y2軸ラベル
 
 # 凡例
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 ax1.legend(h1+h2, l1+l2, loc='lower right')
 # plt.show()
+
+
+# streamlitで表示
 st.pyplot(fig)
-
-# print(moving_average('^DJI').head(20))
-# print(moving_average('^N225').head(20))
-# print(ma_df)
-# print(ma_df.fillna(method='ffill'))
-
-# ma_df.to_csv('/Volumes/外付けSSD1/python/trade/trend/with_na.csv')
-# ma_df.fillna(method='ffill').to_csv('/Volumes/外付けSSD1/python/trade/trend/no_na.csv'
+st.subheader('直近5日間の終値')
+st.dataframe(df_stock.loc[:,['^DJI','^NKX','^TPX']].tail(5))
